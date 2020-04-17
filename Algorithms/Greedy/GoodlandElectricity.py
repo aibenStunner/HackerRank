db10ec9b66553dfc964edbe6e2c8dc66ad866331
Gadri Ebenezer <ebenezergadri99@gmail.com>
2020-04-17 14:44:29 +0000

    Create GoodlandElectricity.py

---
+++ Algorithms/Greedy/GoodlandElectricity.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the pylons function below.
+def pylons(k, arr):
+    n = len(arr)
+    i, j, nTowers, loc = 0, 0, 0, 0
+    while (i < n):
+        nTowers += 1
+        j = i + k - 1
+        if (j > n):
+            j = n - 1
+        while (loc <= j < n and arr[j] == 0):
+            j -= 1
+        if (j < loc):
+            return -1
+        else:
+            loc = j + 1
+            j += k
+            i = j
+    return nTowers
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pylons(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
