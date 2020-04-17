d32421225e19a790db8e8bf931b8a6a13d182002
Gadri Ebenezer <ebenezergadri99@gmail.com>
2020-04-17 13:15:12 +0000

    Create BirthdayCakeCandles.py

---
+++ Algorithms/warmup/BirthdayCakeCandles.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    n = max(ar)
+
+    return ar.count(n)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
