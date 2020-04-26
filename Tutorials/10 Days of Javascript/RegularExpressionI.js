85bbecdfc99c33775920cdc98360c79bf744a1b8
Gadri Ebenezer <ebenezergadri99@gmail.com>
2020-04-26 21:15:11 +0000

    Update RegularExpressionI.js

--- Tutorials/10 Days of Javascript/RegularExpressionI.js
+++ Tutorials/10 Days of Javascript/RegularExpressionI.js
@@ -27,7 +27,15 @@ function regexVar() {
      * Declare a RegExp object variable named 're'
      * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
      */
-    var re = /^([aieou]).*\1$/;
+    var re = /^([aeiou]).*\1$/;
+    //  ^ => first item matches:
+    // () => stores matching value captured within
+    // [aeiou] => matches any of the characters in the brackets
+    // . => matches any character:
+    // * => for 1 or more occurrances (this ensures str length > 3)
+    // \1 => matches to previously stored match. 
+    // \2 looks for matched item stored 2 instances ago.. etc.
+    //  $ ensures that matched item is at end of the sequence
     
     /*
      * Do not remove the return statement
